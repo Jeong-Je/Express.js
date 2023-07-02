97af9bb0d348160f3b30603322581c90ecb3b9b4
Jeong-Je <jeongjeson656@gmail.com>
2023-07-02 19:38:50 +0900

    Register 기능 추가

--- Node.js_게시판/routes/register.js
+++ Node.js_게시판/routes/register.js
@@ -1,9 +1,51 @@
 const express = require('express');
-const app = express();
+const { User } = require('../models');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if(req.session.user){
+        res.send(`<script>
+        alert('잘못된 접근입니다.')
+        documnet.location.href="/";
+        </script>`)
+    }else{
+        res.render('register');
+    }
+})
 
+router.post('/', async(req,res) => {
+    const id = req.body.id;
+    const pwd = req.body.pwd;
+    const pwd2 = req.body.pwd2;
+    if(pwd !== pwd2){
+        res.send(`<script>
+        alert('비밀번호가 일치하지 않습니다.');
+        document.location.href='/register';
+        </script>`)
+    }else{
+        const user =  await User.findOne({
+            where: {
+                username: id,
+            },
+        })
+
+        if(user === null){
+            console.log('사용가능한 아이디');
+            User.create({
+                username: id,
+                password: pwd
+            })
+            res.send(`<script>
+            alert('회원가입이 완료되었습니다.');
+            document.location.href='/login';
+            </script>`)
+        }else{
+            res.send(`<script>
+            alert('이미 등록된 아이디입니다.');
+            document.location.href='/register';
+            </script>`)
+        }
+    }
 })
 
 
