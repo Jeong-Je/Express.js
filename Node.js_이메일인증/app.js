32cb94c92cd4678856e1e9dca8a494853e9d61ab
Jeong-Je <jeongjeson656@gmail.com>
2023-07-12 14:34:06 +0900

    first commit

---
+++ Node.js_이메일인증/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+
+const authRouter = require('./auth');
+app.use(express.json());
+app.use(express.urlencoded( {extended : false} )); 
+
+app.set('port', process.env.PORT || 3002);
+app.set('view engine', 'ejs');
+app.set('views', './');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/mail', authRouter);
+
+app.listen(app.get('port'), () =>{
+    console.log(`${app.get('port')}번 포트에서 대기 중.`)
+})
+
+app.get('/', (req, res) =>{
+    res.render('main');
+})
---
+++ Node.js_이메일인증/auth.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const dotenv = require('dotenv');
+const crypto = require('crypto');
+
+dotenv.config();
+
+const router = express.Router();
+
+router.post('/', async(req, res) => {
+    const userEmail = req.body.email;
+    const nodemailer = require("nodemailer");
+    
+    const authCode = crypto.randomBytes(3).toString('hex');
+
+    let emailTemplete;
+    ejs.renderFile('./template.ejs', { userEmail:userEmail, authCode: authCode}, (err, data) =>{
+        if(err){console.log(err)};
+         emailTemplete = data;
+    })
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS
+      }
+    });
+    
+    async function main() {
+      const info = await transporter.sendMail({
+        from: process.env.NODEMAILER_USER, // sender address
+        to: userEmail, // list of receivers
+        subject: "인증번호를 입력해 이메일 인증을 해주세요.", // Subject line
+        html: emailTemplete, // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+    }
+    
+    main().catch(console.error);
+
+    res.redirect('/');
+});
+
+
+
+module.exports = router;
